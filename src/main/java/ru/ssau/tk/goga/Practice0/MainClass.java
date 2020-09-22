f4dbc0d5826884873feb8edb205e406aa4c42b1d
Chernyshev <Chernyshev.st17@gmail.com>
2020-09-22 23:08:11 +0400

    Выполнено задание 1.3 из списка задач

--- src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
@@ -3,23 +3,8 @@ package ru.ssau.tk.goga.Practice0;
 public class MainClass {
 
     public static void main(String[] args) {
-        Person Mary = new Person();
-        Mary.setFirstName("Mary");
-        Mary.setLastName("Mort");
-        Mary.setPassportID(2340523);
-        System.out.println(Mary.getFirstName()+'\t'+Mary.getLastName()+'\t'+Mary.getPassportID());
-
-        Person Mark = new Person("Mark", "Morten");
-        Mark.setPassportID(3409726);
-        System.out.println(Mark.getFirstName()+'\t'+Mark.getLastName()+'\t'+Mark.getPassportID());
-
-
-        Person Marty = new Person(5360795);
-        Marty.setFirstName("Marty");
-        Marty.setLastName("Loan");
-        System.out.println(Marty.getFirstName()+'\t'+Marty.getLastName()+'\t'+Marty.getPassportID());
-
-        Person Mango = new Person("Mango", "Potato", 123);
-        System.out.println(Mango.getFirstName()+'\t'+Mango.getLastName()+'\t'+Mango.getPassportID());
+        Point firstPoint = new Point(1,1,1);
+        Point secondPoint = new Point(0,0,0);
+        Point thirdPoint = new Point(3,4,5);
     }
 }
---
+++ src/main/java/ru/ssau/tk/goga/Practice0/Point.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.goga.Practice0;
+
+public class Point {
+
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x,double y ,double z ){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
