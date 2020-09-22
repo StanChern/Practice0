bbca97ff1d6fdc00b4b543eb789010a1b777c54e
Chernyshev <Chernyshev.st17@gmail.com>
2020-09-22 22:39:56 +0400

    Выполнено задание 1.2 из списка задач

--- src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
@@ -7,14 +7,19 @@ public class MainClass {
         Mary.setFirstName("Mary");
         Mary.setLastName("Mort");
         Mary.setPassportID(2340523);
-        System.out.println(Mary.getFirstName()+' '+Mary.getLastName());
-        Person Mark = new Person();
-        Mark.setFirstName("Mark");
-        Mark.setLastName("Morten");
+        System.out.println(Mary.getFirstName()+'\t'+Mary.getLastName()+'\t'+Mary.getPassportID());
+
+        Person Mark = new Person("Mark", "Morten");
         Mark.setPassportID(3409726);
-        Person Marty = new Person();
+        System.out.println(Mark.getFirstName()+'\t'+Mark.getLastName()+'\t'+Mark.getPassportID());
+
+
+        Person Marty = new Person(5360795);
         Marty.setFirstName("Marty");
         Marty.setLastName("Loan");
-        Marty.setPassportID(5360795);
+        System.out.println(Marty.getFirstName()+'\t'+Marty.getLastName()+'\t'+Marty.getPassportID());
+
+        Person Mango = new Person("Mango", "Potato", 123);
+        System.out.println(Mango.getFirstName()+'\t'+Mango.getLastName()+'\t'+Mango.getPassportID());
     }
 }
