b8b45f3c7e5d5331d91f43d91156f78cb63d46e0
Chernyshev <Chernyshev.st17@gmail.com>
2020-09-23 12:54:39 +0400

    Выполнено задание 1.5 из списка задач

--- src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
@@ -8,14 +8,8 @@ public class MainClass {
         Point secondPoint = new Point(0,0,0);
         Point thirdPoint = new Point(3,4,5);
 
-        Point resultPoint1 = Points.sum(firstPoint, thirdPoint);
-        Point resultPoint2 = Points.subtract(firstPoint, thirdPoint);
-        Point resultPoint3 = Points.multiply(firstPoint, thirdPoint);
-        Point resultPoint4 = Points.divide(firstPoint, thirdPoint);
+        Point resultPoint1 = Points.enlarge(thirdPoint, 6);
 
-        System.out.println("sum:\t\t" + resultPoint1.x + ' '+ resultPoint1.y + ' ' + resultPoint1.z);
-        System.out.println("subtract:\t" + resultPoint2.x + ' '+ resultPoint2.y + ' ' + resultPoint2.z);
-        System.out.println("multiply:\t" + resultPoint3.x + ' '+ resultPoint3.y + ' ' + resultPoint3.z);
-        System.out.println("divide:\t\t" + resultPoint4.x + ' '+ resultPoint4.y + ' ' + resultPoint4.z);
+        System.out.println("enlarge:" + resultPoint1.x + ' '+ resultPoint1.y + ' ' + resultPoint1.z);
     }
 }
