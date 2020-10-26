05f038328517566fa3fdf9bdfaa2311af6ad44ae
Chernyshev <Chernyshev.st17@gmail.com>
2020-10-26 17:49:07 +0400

    1.14 Выполнено(вроде).

--- src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/MainClass.java
@@ -5,28 +5,16 @@ public class MainClass {
     public static void main(String[] args) {
         Person person = new Person();
         person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        /*1.вызываем checkInt(number), который делает нам нам sout(10).
-        Значение поля number поменяется только внутри выполнения этого
-        метода(т.е. начение копии number), а глобально для всей программы number останется = 5   */
-        checkPerson(person);
-        /*2. в этом случае поле firstName класса person изменится, т.к. создавалась не копия
-        объекта, а копия ссылки на объект
-         */
-        System.out.println(number);
-        // Выводим получаем 5
+        checkAnotherPerson(person);
+        /*Тут в методе checkAnotherPerson() создали нового человека, присвоили ему имя и вывели;
+        Аркадия не трогали совсем */
         System.out.println(person.getFirstName());
-        // Выводим получаем Олега
+        // Вывели Аркадия
     }
 
-    private static void checkInt(int number) {
-        number = 10;
-        System.out.println(number);
-    }
-
-    private static void checkPerson(Person person) {
-        person.setFirstName("Oleg");
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
         System.out.println(person.getFirstName());
     }
 }
