dc89eb4a6ca8bbe8f674a53d34fc727595f318ee
Chernyshev <Chernyshev.st17@gmail.com>
2020-12-08 00:38:51 +0400

    2.4 Выполнено.

--- src/test/java/ru/ssau/tk/goga/Practice0/Arrays/MyArrayTest.java
+++ src/test/java/ru/ssau/tk/goga/Practice0/Arrays/MyArrayTest.java
@@ -13,15 +13,24 @@ public class MyArrayTest {
         assertEquals(MyArray.createArray(1), new int[1]);
     }
 
+    @Test
     public void testGenerateOnesArray() {
         assertEquals(MyArray.generateOnesArray(2), new int[]{2, 2});
         assertEquals(MyArray.generateOnesArray(1), new int[]{2});
         assertEquals(MyArray.generateOnesArray(5), new int[]{2, 1, 1, 1, 2});
     }
 
+    @Test
     public void testGenerateOddNumbers() {
         assertEquals(MyArray.generateOddNumbers(2), new int[]{1,3});
         assertEquals(MyArray.generateOddNumbers(1), new int[]{1});
         assertEquals(MyArray.generateOddNumbers(5), new int[]{1,3,5,7,9});
+    }
+
+    @Test
+    public void testGenerateEvenNumbers() {
+        assertEquals(MyArray.generateFlippedEvenNumbers(2), new int[]{4, 2});
+        assertEquals(MyArray.generateFlippedEvenNumbers(1), new int[]{2});
+        assertEquals(MyArray.generateFlippedEvenNumbers(5), new int[]{10, 8, 6, 4, 2});
     }
 }
