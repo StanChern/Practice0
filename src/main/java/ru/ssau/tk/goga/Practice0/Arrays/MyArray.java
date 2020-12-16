577f7419f4fede015831d5ecc8a41f05f7dee289
Chernyshev <Chernyshev.st17@gmail.com>
2020-12-16 16:08:07 +0400

    2.26 Выполнено

--- src/main/java/ru/ssau/tk/goga/Practice0/Arrays/MyArray.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/Arrays/MyArray.java
@@ -266,4 +266,12 @@ public class MyArray {
         }
         return arrayBitNot;
     }
+
+    static int[] getPairSum(int[] array) {
+        int[] _1array = new int[array.length / 2 + (array.length % 2 == 0 ? 0 : 1)];
+        for (int i = 0; i < _1array.length; i++) {
+            _1array[i] = array[2 * i] + (2 * i + 1 < array.length ? array[2 * i + 1] : 0);
+        }
+        return _1array;
+    }
 }
--- src/test/java/ru/ssau/tk/goga/Practice0/Arrays/MyArrayTest.java
+++ src/test/java/ru/ssau/tk/goga/Practice0/Arrays/MyArrayTest.java
@@ -197,4 +197,10 @@ public class MyArrayTest {
         assertEquals(arrayBitNot[0], 254);
         assertEquals(arrayBitNot[1], 32);
     }
+
+    @Test
+    public void testGetPairSum() {
+        assertEquals(MyArray.getPairSum(new int[]{10, 4, 8, 2, 9}), new int[]{14, 10, 9});
+        assertEquals(MyArray.getPairSum(new int[]{10, 4, 8, 2, 9, 1, 2, 5}), new int[]{14, 10, 10, 7});
+    }
 }
