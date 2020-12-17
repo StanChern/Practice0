f0629586739e22c36e732bafae55995d1a241c6d
Chernyshev <Chernyshev.st17@gmail.com>
2020-12-17 16:32:33 +0400

    3.6 Выполнено

--- src/main/java/ru/ssau/tk/goga/Practice0/Strings/MainClass.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/Strings/MainClass.java
@@ -3,11 +3,7 @@ package ru.ssau.tk.goga.Practice0.Strings;
 public class MainClass {
     public static void main(String[] args) {
 
-        String s1 = "Home";
-        String s2 = "home";
-        String s3 = "хома";
-        System.out.println(MyString.differentInRegister(s1, s2));
-        System.out.println(MyString.differentInRegister(s3, s2));
+        MyString.escapeCharacter();
 
     }
 }
