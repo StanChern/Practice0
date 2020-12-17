58f3aa2938265c019b535c4dc4b6a9ede0897d34
Chernyshev <Chernyshev.st17@gmail.com>
2020-12-17 15:47:05 +0400

    3.2 Выполнено

--- src/main/java/ru/ssau/tk/goga/Practice0/Strings/MainClass.java
+++ src/main/java/ru/ssau/tk/goga/Practice0/Strings/MainClass.java
@@ -2,8 +2,12 @@ package ru.ssau.tk.goga.Practice0.Strings;
 
 public class MainClass {
     public static void main(String[] args) {
-        String s1 = "aBcDeFgH";
-        MyString MyString = new MyString();
-        MyString.splitLine(s1);
+
+        String s1 = "home";
+        String s2 = "дом";
+        MyString.getStringBytes(s1);
+        System.out.println();
+        MyString.getStringBytes(s2);
+
     }
 }
