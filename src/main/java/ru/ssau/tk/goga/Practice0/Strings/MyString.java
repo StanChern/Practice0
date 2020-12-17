79b8ffb205498aa0fc54aabf7d0a5acca05655b0
Chernyshev <Chernyshev.st17@gmail.com>
2020-12-17 15:36:26 +0400

    3.1 Выполнено

---
+++ src/main/java/ru/ssau/tk/goga/Practice0/Strings/MyString.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.goga.Practice0.Strings;
+
+public class MyString {
+
+    void splitLine(String myString) {
+        for (int i = 0; i < myString.length(); i++) {
+            System.out.println(myString.charAt(i));
+        }
+    }
+}
